fef253978bb57904393bb18cc0f5a534ef859749
Federico Ciuffardi <federico.ciuffardi@outlook.com>
2021-10-14 19:59:09 -0300

    Get map error in digest

--- scripts/log_digester.py
+++ scripts/log_digester.py
@@ -9,7 +9,7 @@ import math
 import pandas as pd
 from collections import defaultdict
 from pygnuplot import gnuplot # dependency
-
+from PIL import Image
 
 ########
 # Vars #
@@ -23,63 +23,78 @@ HOME=os.environ['HOME']
 pkg_dir=f"{HOME}/catkin_ws/src/pgmappingcooperativo"
 log_dir=f"{pkg_dir}/log"
 data_dir=f"{pkg_dir}/data"
+maps_dir=f"{pkg_dir}/maps"
 
 ## params 
 x_param = "cell_size"
-y_params = [ "explored_cells" , "exploration_time" ] # params on termination.yaml
+y_params = [ "exploration_time" ] # params on termination.yaml
 
 #############
 # Functions #
 #############
 
-def linealize_cell_size(data):
-    linealized_data = {}
-    for x_value, y_values in data.items():
-        linealized_data[math.pow(1/x_value,2)] = y_values
-    return linealized_data
+def cell_size_2_cells_per_meter(val):
+    return math.pow(1/val,2)
 
-def process_times(this_log_dir,name):
-    with open(f"{this_log_dir}/{name}_time", 'r') as f:
+def cell_size_dict_2_cells_per_meter_dict(cell_size_dict):
+    cells_per_meter_dict = {}
+    for x_value, y_values in cell_size_dict.items():
+        cells_per_meter_dict[cell_size_2_cells_per_meter(x_value)] = y_values
+    return cells_per_meter_dict
+
+def set_predigest_value(predigest, x_value , y_param, y_value):
+    predigest.setdefault(y_param,{})
+    predigest[y_param].setdefault(x_value,[])
+    predigest[y_param][x_value].append(y_value)
+
+def process_times(predigest, this_log_dir, x_value, name):
+    times = []
+    with open(f"{this_log_dir}/{name}", 'r') as f:
         lines_list = f.readlines()
-        time_max = 0
-        time_sum = 0
-        time_count = 0
         for line in lines_list:
-            time = float(line.split()[1])
-            time_sum += time
-            if(time_max < time):
-                time_max = time
-            time_count += 1
+            times.append(float(line.split()[1]))
 
-    predigest.setdefault(name+"_time_max",{})
-    predigest[name+"_time_max"].setdefault(x_value,[])
-    predigest[name+"_time_max"][x_value].append(time_max)
+    set_predigest_value(predigest, x_value, name+"_max", max(times))
 
-    predigest.setdefault(name+"_time_mean",{})
-    predigest[name+"_time_mean"].setdefault(x_value,[])
-    predigest[name+"_time_mean"][x_value].append(time_sum/time_count)
+    time_sum = sum(times)
+    set_predigest_value(predigest, x_value, name+"_sum",  time_sum)
 
-    # times increment
-    with open(f"{this_log_dir}/{name}_time_diff", 'r') as f:
-        lines_list = f.readlines()
-        time_increment_max = 0
-        time_increment_sum = 0
-        time_increment_count = 0
-        for line in lines_list:
-            time_increment = float(line.split()[1])
-            time_increment_sum += time_increment
-            if(time_increment_max < time_increment):
-                time_increment_max = time_increment
-            time_increment_count += 1
+    set_predigest_value(predigest, x_value, name+"_mean", time_sum/len(times))
 
-    predigest.setdefault(name+"_time_increment_max",{})
-    predigest[name+"_time_increment_max"].setdefault(x_value,[])
-    predigest[name+"_time_increment_max"][x_value].append(time_increment_max)
+def isUnk(val):
+    return val == 205
 
-    predigest.setdefault(name+"_time_increment_mean",{})
-    predigest[name+"_time_increment_mean"].setdefault(x_value,[])
-    predigest[name+"_time_increment_mean"][x_value].append(time_increment_sum/time_increment_count)
+def cell_size_to_map_name(cell_size):
+    map_name = ""
+    for c in "{:.3f}".format(cell_size):
+        if c == '.':
+            map_name += '_'
+        else:
+            map_name += c 
 
+    return map_name+"m.pgm"
+
+def compare_maps(real_path, explored_path): 
+    real_pixels     = list(Image.open(real_path).getdata())
+    explored_pixels = list(Image.open(explored_path).getdata())
+    
+    if len(real_pixels) != len(explored_pixels):
+        sys.exit(f"real map ({real_path}) and explored map ({explored_path}) dimensions do not match")
+
+    erroneous_cells = 0
+    real_map_cells = 0
+    explored_map_cells = 0
+    for (real_pixel, explored_pixel) in zip(real_pixels,explored_pixels):
+        if isUnk(real_pixel): continue # skip if the real map has a value of unknown
+
+        real_map_cells += 1
+        if real_pixel != explored_pixel:
+            erroneous_cells += 1
+
+        if not isUnk(explored_pixel):
+            explored_map_cells += 1
+
+    return real_map_cells, explored_map_cells, erroneous_cells
 
 ########
 # MAIN #
@@ -100,44 +115,73 @@ os.mkdir(digest_dir)
 predigest = {} 
 
 for this_log in os.scandir(log_dir):
+    # set the full path to the current log dir
     this_log_dir = f"{log_dir}/{this_log.name}"
 
-    if(not os.path.isfile(f"{this_log_dir}/termination.yaml")):
+    # check if file is valid
+    if this_log.name == 'latest':
+        continue
+    if not os.path.isfile(f"{this_log_dir}/termination.yaml"):
         print(f"{this_log_dir} not terminated successfully")
         continue
 
+    # load rosparams yaml
     with open(f"{this_log_dir}/rosparams.yaml", 'r') as f:
         rosparams_yaml = yaml.safe_load(f)
 
+    # get the x_value
     x_value = rosparams_yaml[x_param]
 
-    # params from termination.yamls
+    # load termination yaml
     with open(f"{this_log_dir}/termination.yaml", 'r') as f:
         termination_yaml = yaml.safe_load(f)
-    for y_param in y_params:
-        predigest.setdefault(y_param,{})
-        predigest[y_param].setdefault(x_value,[])
-        predigest[y_param][x_value].append(termination_yaml[y_param])
 
-    # distance
+    # set the y_values (from termination.yaml) in the predigest 
+    for y_param in y_params:
+        set_predigest_value(predigest, x_value, y_param, termination_yaml[y_param])
+
+    # set the distance sum in the predigest
+    meters_traveled_list = []
     with open(f"{this_log_dir}/meters_traveled", 'r') as f:
         lines_list = f.readlines()
-        meters_traveled_list = []
         for val in lines_list:
             meters_traveled_list.append(float(val))
 
-    meters_traveled_sum =  sum(meters_traveled_list)
+    exploration_cost = sum(meters_traveled_list) # the sum of the distance travelled by the robots
+    set_predigest_value(predigest, x_value, "exploration_cost", exploration_cost)
 
-    predigest.setdefault("distance_sum",{})
-    predigest["distance_sum"].setdefault(x_value,[])
-    predigest["distance_sum"][x_value].append(meters_traveled_sum)
+    # get map name
+    map_name = rosparams_yaml['map_name']
 
-    # predigest.setdefault("distance_mean",{})
-    # predigest["distance_mean"].setdefault(x_value,[])
-    # predigest["distance_mean"][x_value].append(meters_traveled_sum/len(meters_traveled_list))
+    # get map info
+    real_map_path     = f"{maps_dir}/{map_name}/{cell_size_to_map_name(x_value)}"
+    explored_map_path = f"{this_log_dir}/map.pgm" 
+    real_cells, explored_cells, erroneous_cells = compare_maps(real_map_path, explored_map_path)
 
-    process_times(this_log_dir, 'auction_info')
-    process_times(this_log_dir, 'gvd_construction')
+    # set the explored_area in the predigest
+    explored_area = explored_cells/cell_size_2_cells_per_meter(x_value) 
+    set_predigest_value(predigest, x_value, "explored_area", explored_area)
+
+    # set the exploration_efficiency in the predigest
+    set_predigest_value(predigest, x_value, "exploration_efficiency", explored_area/exploration_cost)
+
+    # set the erroneous_area in the predigest
+    erroneous_area = erroneous_cells/cell_size_2_cells_per_meter(x_value)
+    set_predigest_value(predigest, x_value, "erroneous_area", erroneous_area)
+
+    # set the map_completeness in the predigest
+    real_area = real_cells/cell_size_2_cells_per_meter(x_value)
+    set_predigest_value(predigest, x_value, "map_completeness", explored_area/real_area)
+
+    # set the map_quality in the predigest
+    set_predigest_value(predigest, x_value, "map_quality", (explored_area-erroneous_area)/real_area)
+
+    # set time related values in the predigest
+    process_times(predigest, this_log_dir, x_value, 'auction_info_time')
+    process_times(predigest, this_log_dir, x_value, 'auction_info_time_diff')
+
+    process_times(predigest, this_log_dir, x_value, 'gvd_construction_time')
+    process_times(predigest, this_log_dir, x_value, 'gvd_construction_time_diff')
 
 # Paso 2 armar los digest de cada predigest
 
@@ -184,10 +228,10 @@ for y_param, stats in digest.items():
                         ylabel = f"\"{y_param.replace('_',' ')}\"",
                         yrange = "[0:]")
 
-    mean_df    = pd.DataFrame.from_dict(linealize_cell_size(stats["mean"]), orient='index')
-    std_dev_df = pd.DataFrame.from_dict(linealize_cell_size(stats["std_dev"]), orient='index')
+    mean_df    = pd.DataFrame.from_dict(cell_size_dict_2_cells_per_meter_dict(stats["mean"]), orient='index')
+    std_dev_df = pd.DataFrame.from_dict(cell_size_dict_2_cells_per_meter_dict(stats["std_dev"]), orient='index')
     
     df = mean_df.join(std_dev_df, lsuffix="mean", rsuffix="std_dev", sort = True)
 
-    g.set("offset graph 0.02,graph 0.02,0,0 ")
+    g.set("offset graph 0.02,graph 0.02,0.02,0 ")
     g.plot_data(df,'using 1:2 notitle with lines lw 0.5 lc rgb \'grey\'','using 1:2:3 notitle with yerrorbars lc rgb \'blue\' pt 1')
